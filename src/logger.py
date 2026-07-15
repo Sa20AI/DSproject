4be2e6b71718b0c500383fe32be4dbf2f47fdc7e
Sa20AI <sanskarunkule02@gmail.com>
2026-07-15 19:56:48 +0530

    Updated Exception and Logger

--- src/logger.py
+++ src/logger.py
@@ -3,14 +3,17 @@ import os
 from datetime import datetime
 import sys
 
-from src.exception import CustomException
+try:
+    from src.exception import CustomException
+except ModuleNotFoundError:
+    from exception import CustomException
 
 
-LOG_FILE=f"{datetime.now().strftime('%m%d%Y__%H%M%S')}.log"
-log_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(log_path,exist_ok=True)
+LOG_FILE = f"{datetime.now().strftime('%m%d%Y__%H%M%S')}.log"
+log_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(log_path, exist_ok=True)
 
-LOG_FILE_PATH=os.path.join(log_path,LOG_FILE)
+LOG_FILE_PATH = os.path.join(log_path, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
@@ -19,10 +22,3 @@ logging.basicConfig(
 
 )
 
-if __name__=="__main__":
-
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by Zero")
-        raise CustomException(e, sys.exc_info())
